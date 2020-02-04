ff3df86667b597b6d56a234aefa5270ba238de22
irene macapinlac <irenekaymac@gmail.com>
2020-02-04 15:38:56 +1100

    show image when image is uploaded

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -4,22 +4,24 @@ import {Image, Video, Transformation, CloudinaryContext} from 'cloudinary-react'
 class Upload extends React.Component {
 
   state = {
-    publicId: ''
+    image_url: ''
   };
 
   uploadWidget = () => {
 
-    window.cloudinary.openUploadWidget({ cloud_name: 'dlbfbi0rp', upload_preset: 'mtkzwbw5', tags:['']},
+    window.cloudinary.openUploadWidget({
+
+      cloud_name: 'dlbfbi0rp', upload_preset: 'mtkzwbw5', tags:['plants']},
       (error, result) => {
 
           if(!error){
             console.log('succefully-uploaded', result);
-            this.setState({ publicId: result[0].public_id });
+            this.setState({ image_url: result[0].url });
             // this.props.reportUploadId( result[0].public_id } );
           } else {
             console.warn('ERROR UPLOADING', error);
           }
-          
+
       });
   }
 
@@ -29,9 +31,17 @@ class Upload extends React.Component {
 
         <div className="upload">
 
+
                 <button onClick={this.uploadWidget} className="upload-button">
                     Upload PLANT!
                 </button>
+                {
+                  this.state.image_url.length === 0
+                  ?
+                  <p>noimage</p>
+                  :
+                  <img className="uploadImagePreview" src={this.state.image_url} />
+                }
 
             </div>
 
