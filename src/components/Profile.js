aa7476dca3c27ce8e01af59c2a596807b8227e7d
Tim Hobson <timhobson85@gmail.com>
2020-02-04 11:13:42 +1100

    fixed all pages

---
+++ src/components/Profile.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import axios from 'axios'
+
+let userIDQuery = ''
+
+class Profile extends React.Component {
+
+  handleClick = ( id ) => {
+    console.log(id);
+    const route = `/photo/${ id }`
+    console.log('route:', route);
+    this.props.history.push( route )
+}
+
+  state = {
+    user: []
+  }
+
+
+  componentDidMount(){
+    // console.log('this.props.match.params.user_id', this.props.match.params.user_id);
+    const token = localStorage.getItem('token')
+    if (token){
+      console.log('TOKEN FOUND!', token);
+      axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+      // setTokenHeaderSet( true );
+    }
+    userIDQuery = this.props.match.params.user_id
+    console.log(`userIDQuery`, userIDQuery);
+    axios.get(`http://localhost:3000/users/${userIDQuery}.json`)
+    .then(res => {
+      this.setState({user: res.data})
+      console.log('this.state.user', this.state.user);
+      console.log(this.state.user.plants.length);
+    })
+    .catch(console.warn)
+  }
+
+  render(){
+    return(
+      <div className='App'>
+        <h2>Profile</h2>
+        <p>{this.state.user.name}</p>
+        {
+          this.state.user.length === 0
+          ?
+          <p>loading</p>
+          :
+          <div>
+          <p>plants</p>
+          {
+            this.state.user.plants.map(plant =>
+              <div key={plant.id} className="profilePlantIndex">
+                <p>{plant.name}</p>
+                {
+                  plant.photos.map( photo =>
+                    <div className="profilePlantPhoto" onClick={() => this.handleClick(photo.id)}>
+                      <img src={photo.image}/>
+                    </div>
+                  )
+                }
+              </div>
+            )
+          }
+          </div>
+        }
+      </div>
+    )
+  }
+
+} // Class
+
+export default Profile
