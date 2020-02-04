073706e11fd73e4f078347c5b8425686a7daeda4
Sreevani Pasarlapati <sreevani.pasarla@gmail.com>
2020-02-05 10:25:52 +1100

    Calendar code cleanup

--- src/components/PlantCalendar.js
+++ src/components/PlantCalendar.js
@@ -1,85 +1,38 @@
 import React, {Component} from 'react';
 import axios from 'axios';
 import moment from 'moment';
-<<<<<<< HEAD
 import { Calendar, momentLocalizer, Views  } from 'react-big-calendar';
-import 'react-big-calendar/lib/css/react-big-calendar.css'
-import '../App.css'
-=======
-import DayPicker, { DateUtils } from 'react-day-picker';
-import 'react-day-picker/lib/style.css';
->>>>>>> f0eb132df8eae59e476c94aabdb67d40b89d86c9
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import '../App.css';
 
-
-let waterInterval = 0;
-
-class WaterInterval extends React.Component {
-
-  handleChange = (event) => {
-    waterInterval = event.target.value;
-  };
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-
-    this.props.onSubmit(waterInterval);
-  }; // handleSubmit
-
-  render() {
-    return(
-      <div>
-        <form onSubmit={ this.handleSubmit }>
-          <label> Watering Interval (in days) </label>
-          <input type="text" onChange={this.handleChange} /> <br />
-          <input type="submit" value="Update" />
-        </form>
-      </div>
-    );  // return
-  } // render
-} // WaterInterval
 
 class PlantCalendar extends React.Component {
 
   state = {
-<<<<<<< HEAD
     user: [],
-    id: localStorage.getItem('userId'),
-=======
->>>>>>> f0eb132df8eae59e476c94aabdb67d40b89d86c9
-    date: new Date(),
-    acquiredDate: new Date("2019-12-28"),
-    datesArray: [],
-    data: false
+    userId: localStorage.getItem('userId'),
+    datesArray: []
   }
 
-  onChange = date => this.setState({date});
-<<<<<<< HEAD
-
   componentDidMount(){
-    axios.get(`http://localhost:3000/users/${this.state.id}.json`)
-    .then( res => {
-      this.setState({
-        user: res.data
-      });
 
+    axios.get(`http://localhost:3000/users/${this.state.userId}.json`)
+    .then( res => {
+      this.setState({ user: res.data });
       const monthStartDate = new Date();
       monthStartDate.setDate(1);
       this.state.user.plants.forEach(plant => {
         console.log("###", plant.date_acquired,plant.name);
         this.calculate( plant.name, plant.date_acquired, plant.water_days, monthStartDate );
       });
-      // console.log('PLANTS**: ', this.state.user.plants);
-    });
+    })
+    .catch(err => console.warn(err));
   }
 
   calculate = (plantName, startDate, waterInterval, currMonthStart) => {
 
     // this.state.datesArray = [];
     let acquiredDate = new Date(startDate);
-    if(waterInterval <= 0) {
-      this.setState({data: true});
-      return;
-    }
 
     const tempDate = acquiredDate;
     console.log(tempDate);
@@ -87,21 +40,6 @@ class PlantCalendar extends React.Component {
     if(currMonthStart < startDate) {
       this.state.datesArray = [];
       console.log("RETURN",currMonthStart,tempDate);
-=======
-  // axios.get('http://localhost:3000/plants')
-  // .then(res => {
-  //   console.log(res);
-  //
-  //   // this.setState({acquiredDate: res.data});
-  // })
-  // .catch(err => console.warn(err));
-
-  calculate = (startDate, waterInterval, currMonthStart) => {
-
-    if(waterInterval <= 0) {
-      this.setState({data: true});
-      this.state.datesArray = [];
->>>>>>> f0eb132df8eae59e476c94aabdb67d40b89d86c9
       return;
     }
 
@@ -117,16 +55,11 @@ class PlantCalendar extends React.Component {
     let firstScheduleDt = moment(currMonthStart).add( offset, 'days' ).toDate();
 
     let nextScheduleDt = firstScheduleDt;
-<<<<<<< HEAD
     // this.state.datesArray = [];
-=======
-    this.state.datesArray = [];
->>>>>>> f0eb132df8eae59e476c94aabdb67d40b89d86c9
 
     //TODO: loop until end of month
     while(nextScheduleDt.getMonth() === firstScheduleDt.getMonth()){
 
-<<<<<<< HEAD
       let calendarEntry = {
         title: plantName,
         allDay: true,
@@ -134,23 +67,20 @@ class PlantCalendar extends React.Component {
         end: nextScheduleDt,
       }
       if( nextScheduleDt.getTime() > acquiredDate.getTime() ) {
-        console.log("### IN IF");
         this.state.datesArray.push(calendarEntry);
       }
       nextScheduleDt = moment(nextScheduleDt).add(waterInterval, 'days').toDate();
     }
-    // console.log("DATES ARR:", this.state.datesArray);
 
     this.setState({data: true});
   } // calculate
 
+  // This calculates watering dates of plants when month view is changed in Calendar.
   monthsChangeHandler = (date) => {
 
     this.state.datesArray = [];
     const startDate = date.start;
 
-    console.log('MONTH: ', date);
-    console.log(typeof(startDate));
     if(startDate.getDate() !== 1) {
       startDate.setMonth(startDate.getMonth()+1);
       startDate.setDate(1);
@@ -171,37 +101,6 @@ class PlantCalendar extends React.Component {
             onRangeChange={this.monthsChangeHandler}
             views={Views.MONTH}
           />
-=======
-      this.state.datesArray.push(nextScheduleDt);
-      nextScheduleDt = moment(nextScheduleDt).add(waterInterval, 'days').toDate();
-
-    }
-
-    this.setState({data: true});
-  }
-
-  monthsChangeHandler = (month) => {
-
-    this.calculate(this.state.acquiredDate, waterInterval, month);
-  }
-
-  render() {
-    return(
-      <div>
-        <br />
-        <WaterInterval onSubmit={(waterInterval) => {
-          this.state.date.setDate(1);
-          this.calculate(this.state.acquiredDate, waterInterval, this.state.date)
-        }}
-        />
-        {
-          this.state.data
-            ? < DayPicker selectedDays={this.state.datesArray}
-                  onMonthChange = {this.monthsChangeHandler}/>
-            : null
-        }
-        <br />
->>>>>>> f0eb132df8eae59e476c94aabdb67d40b89d86c9
       </div>
     );  // return
   } // render
