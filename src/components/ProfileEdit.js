62e40a016e32d962c085a8e5fe78afa680bbe369
irene macapinlac <irenekaymac@gmail.com>
2020-02-09 00:22:13 +1100

    Update ProfileEdit.js

--- src/components/ProfileEdit.js
+++ src/components/ProfileEdit.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import './../SignUp.css'
 
 class ProfileEdit extends React.Component{
@@ -12,6 +13,26 @@ class ProfileEdit extends React.Component{
     password_confirmation: 'chicken'
   };
 
+  handleChange = (event) => {
+    const name = event.target.name;
+    const newValue = event.target.value;
+
+    this.setState({[name]: newValue});
+
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    const token = localStorage.getItem('token')
+
+    axios.patch('http://localhost:3000/users')
+    .then(res => {
+      console.log(`res:`, res);
+    })
+    .then( console.log('this.props.match.params.user_id', this.props.match.params.user_id) )
+    .catch(console.warn)
+  }
+
   render(){
     return(
 
@@ -19,46 +40,46 @@ class ProfileEdit extends React.Component{
 
         <h2 class="formTitle">Edit Profile</h2>
 
-          <form /* onSubmit={handleSubmit}*/>
+          <form onSubmit={this.handleSubmit}>
             <div className="field">
               <label>Name:</label>
               <br/>
-              <input value={this.name} /*onChange={handleChange}*/ type="text" name="name" placeholder="name"/>
+              <input defaultValue={this.state.name} onChange={this.handleChange} type="text" name="name" placeholder="name"/>
             </div>
             <br/>
 
             <div className="field">
               <label>Username:</label>
               <br/>
-              <input value={this.username} /*onChange={handleChange}*/ type="text" name="username" placeholder="username"/>
+              <input defaultValue={this.state.username} onChange={this.handleChange} type="text" name="username" placeholder="username"/>
             </div>
             <br/>
 
             <div className="field">
               <label>Location:</label>
               <br/>
-              <input value={this.location} /*onChange={handleChange}*/ type="text" name="location" placeholder="location"/>
+              <input defaultValue={this.state.location} onChange={this.handleChange} type="text" name="location" placeholder="location"/>
             </div>
             <br/>
 
             <div className="field">
               <label>Email:</label>
               <br/>
-              <input value={this.email} /*onChange={handleChange}*/ type="text" name="email" placeholder="email"/>
+              <input defaultValue={this.state.email} onChange={this.handleChange} type="text" name="email" placeholder="email"/>
             </div>
             <br/>
 
             <div className="field">
               <label>Password:</label>
               <br/>
-              <input value={this.password} /*onChange={handleChange}*/ type="password" name="password" placeholder="password"/>
+              <input defaultValue={this.state.password} onChange={this.handleChange} type="password" name="password" placeholder="password"/>
             </div>
             <br/>
 
             <div className="field">
               <label>Password Confirmation:</label>
               <br/>
-              <input value={this.password_confirmation} /*onChange={handleChange}*/ type="password" name="password_confirmation" placeholder="password confirmation"/>
+              <input defaultValue={this.state.password_confirmation} onChange={this.handleChange} type="password" name="password_confirmation" placeholder="password confirmation"/>
             </div>
             <br/>
 
