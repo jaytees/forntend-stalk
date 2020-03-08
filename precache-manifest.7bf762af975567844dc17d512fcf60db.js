8e61d3fc5fa817755c8b20f51c28794846097400
Jordan Trickett <jordan1trickett@gmail.com>
2020-03-08 11:52:27 +1100

    Updates

---
+++ precache-manifest.7bf762af975567844dc17d512fcf60db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df199b70a55fb4225ff632581cfb309b",
+    "url": "/frontend-stalk/index.html"
+  },
+  {
+    "revision": "ee4a208dcde86ad75991",
+    "url": "/frontend-stalk/static/css/2.01909d95.chunk.css"
+  },
+  {
+    "revision": "5ea3bd9e96f2f44b0429",
+    "url": "/frontend-stalk/static/css/main.36c50ccd.chunk.css"
+  },
+  {
+    "revision": "ee4a208dcde86ad75991",
+    "url": "/frontend-stalk/static/js/2.ede1cd29.chunk.js"
+  },
+  {
+    "revision": "22f6ec7ef22eb3f84567a8bc5a14549e",
+    "url": "/frontend-stalk/static/js/2.ede1cd29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ea3bd9e96f2f44b0429",
+    "url": "/frontend-stalk/static/js/main.4f05b14b.chunk.js"
+  },
+  {
+    "revision": "dcaf549b7d39163c0dbc",
+    "url": "/frontend-stalk/static/js/runtime-main.b3d67bb2.js"
+  }
+]);
